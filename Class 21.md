87442baa77efae42aac137b0a43662e14bc125c8
Pranav-2008 <71555572+Pranav-2008@users.noreply.github.com>
2020-10-01 10:17:52 +0530

    Add files via upload

---
+++ Class 21.md
@@ -0,0 +1,9 @@
+Class 21
+
+Topic Writing functions which can take arguments
+
+● Learn "true" and "false" as the two boolean values. 
+
+● Write a function which can accept arguments, return values and can be re-used for the different game objects. 
+
+● Create a little code library and use it within the code.
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var fixedRect, movingRect;
+
+var o1,o2,o3,o4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,400,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  o1 = createSprite(600, 200, 50, 50);
+  o1.shapeColor = "green";
+  o1.debug = true;
+  o2 = createSprite(400,200,50,50);
+  o2.shapeColor = "green";
+  o2.debug = true;
+
+  o3 = createSprite(200, 200, 50, 50);
+  o3.shapeColor = "green";
+  o3.debug = true;
+  o4 = createSprite(800,200,50,50);
+  o4.shapeColor = "green";
+  o4.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(collision(movingRect,o2)){
+    movingRect.shapeColor = "red";
+    o2.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    o2.shapeColor = "green";
+  }
+
+  drawSprites();
+}
